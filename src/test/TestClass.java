17e9ad6bf11c4ed48f45ed7bb67c01ba01453446
Pieter Jan <pieterjaninfo@gmail.com>
2017-01-21 00:44:49 +0100

    Test worked! -> going to re-implement old Classes java file now.

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -4,17 +4,22 @@ import main.Classes;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Pieter Jan on 20-1-2017.
  */
 public class TestClass {
 
-    private static Collection<TestClass> classes;
+    private static Set<TestClass> classes = new HashSet<>(); // not initialized
+    private static HashMap<String, TestClass> classes2 = new HashMap<>();
 
     public static Collection<TestClass> getClasses() {
         return classes;
     }
+    private static HashMap<String, TestClass> getClasses2() { return classes2; }
+
 
     public static void printClassesInfo(boolean printwords) {
         System.out.println("[Classes.java] Info of all classes: ");
@@ -29,7 +34,9 @@ public class TestClass {
         this.className = className;
         this.vocabulary = vocabulary;
         classes.add(this);
+//        classes2.put(className, this);
     }
+
 
     private String className;
     private HashMap<String, Integer> vocabulary;    // MAPS WORD -> COUNT
