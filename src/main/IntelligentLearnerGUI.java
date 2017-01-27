7839130dae77c35a6a9824b9cb7c1f5423a38887
JChavannes01 <janko.chavannes@gmail.com>
2017-01-27 12:45:03 +0100

    Added loading indicator to test classifier button (last tab)

--- src/main/IntelligentLearnerGUI.java
+++ src/main/IntelligentLearnerGUI.java
@@ -281,27 +281,50 @@ public class IntelligentLearnerGUI extends Component {
 
         // Test button in Automatic testing tab
         testClassifierButton.addActionListener((ActionEvent e) -> {
-            if (updateCheckBox.isSelected()) {
-                //TODO: update with the test files.
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    ImageIcon loading = new ImageIcon("resources/images/ajax-loader.gif");
+                    testClassifierButton.setIcon(loading);
+                    testClassifierButton.setText("Please wait...");
+                }
+            });
+            //Start training the classifier on a new thread
+            Thread testThread = new Thread() {
+                public void run() {
+                    if (updateCheckBox.isSelected()) {
+                        //TODO: update with the test files.
 
+                    };
+
+                    String testpath = testFc.getSelectedFile().getAbsolutePath() + "\\";
+                    if (debug) System.out.println("TESTPATH: " + testpath);
+
+                    HashMap<String, HashMap<String, Integer>> stats = dc.scanTestDocuments(testpath);
+
+                    List<String> sortedClasses = new ArrayList(stats.keySet());
+                    Collections.sort(sortedClasses);
+
+                    int[][] table = Utils.createTable(stats);
+                    Utils.printTable(table, sortedClasses);
+                    Utils.getStatistics(table, sortedClasses);
+
+                    String log = Utils.getLog();
+                    logTextArea.append(getCurrentTime() + "\n");
+                    logTextArea.append(log);
+                    Utils.resetLog();
+
+                    //Set button back to default text
+                    SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            testClassifierButton.setIcon(null);
+                            testClassifierButton.setText("Test Classifier");
+                        }
+                    });
+                }
             };
-
-            String testpath = testFc.getSelectedFile().getAbsolutePath() + "\\";
-            if (debug) System.out.println("TESTPATH: " + testpath);
-
-            HashMap<String, HashMap<String, Integer>> stats = dc.scanTestDocuments(testpath);
-
-            List<String> sortedClasses = new ArrayList(stats.keySet());
-            Collections.sort(sortedClasses);
-
-            int[][] table = Utils.createTable(stats);
-            Utils.printTable(table, sortedClasses);
-            Utils.getStatistics(table, sortedClasses);
-
-            String log = Utils.getLog();
-            logTextArea.append(getCurrentTime() + "\n");
-            logTextArea.append(log);
-            Utils.resetLog();
+            testThread.start();
         });
     }
 
